7717f09a48305bc96a02f08efcf5d512839e3f1d
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-07-02 20:02:04 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -172,12 +172,12 @@ router.get("/challengedetail/:id", (req,res) => {
 })
 
 //update profile settings
-router.post('/upload', uploader.single("imageUrl"), (req, res, next) => {
-  if (!req.file) {
-    next(new Error('No file uploaded!'));
-    return;
-  }
-  res.json({ secure_url: req.file.secure_url });
-})
+// router.post('/upload', uploader.single("imageUrl"), (req, res, next) => {
+//   if (!req.file) {
+//     next(new Error('No file uploaded!'));
+//     return;
+//   }
+//   res.json({ secure_url: req.file.secure_url });
+// })
 
 module.exports = router;
